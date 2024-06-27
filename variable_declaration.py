837ecd99e610cd11668bfe38e41848e333f58750
gurdittacdac <cadaclearningacts@gmail.com>
2024-06-27 20:13:00 +0530

    variable_declaration.py

---
+++ variable_declaration.py
@@ -0,0 +1,19 @@
+num1 = 20
+
+name = "cdac"
+
+status = True
+
+print(type(num1))
+print(type(name))
+print(type(status))
+
+num2= 102
+sum = num1 + num2
+print(sum)
+
+
+"""
+Student are so tired that they felt 8 PM around 7:57
+
+"""
